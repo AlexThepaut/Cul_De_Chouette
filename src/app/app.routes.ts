472db229ec9397fc7569e615a2edd894c02cf5f1
Alex <alex.thepaut33@gmail.com>
2019-04-01 21:36:37 +0200

    Ajout du resolver pour Dev et changement du constructeur Joueur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { PATH_HOME, PATH_GAME } from './app.routes.constantes';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
+import { DevResolvers } from './resolver/dev.resolver';
 
 export const ROUTES: Routes = [
     { path: PATH_HOME, component: HomeComponent },
-    { path: PATH_GAME, component: GameComponent /*, canActivate: [EnoughPlayerGuard], */ }
+    {
+        path: PATH_GAME, component: GameComponent /*, canActivate: [EnoughPlayerGuard], */,
+        resolve: {
+            joueurs: DevResolvers
+        }
+    }
 ];
