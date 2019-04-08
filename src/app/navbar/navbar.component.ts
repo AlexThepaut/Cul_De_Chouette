cd22b4aba903e8f09e68b6edfc229035dd390bfd
Alex <alex.thepaut33@gmail.com>
2019-04-08 21:18:57 +0200

    Navbar fix des boutons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { PATH_GAME } from '../app.routes.constantes';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isHidden: boolean;
+
+  constructor(private location: Location, private router: Router) { }
 
   ngOnInit() {
+    this.isHidden = this.hadToBeHidden();
   }
 
+  toPrevious(){
+    this.location.back();
+  }
+
+  hadToBeHidden(){
+    return (this.router.url === `/${PATH_GAME}`) ? true : false;
+  }
 }
